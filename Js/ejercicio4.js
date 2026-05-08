b624f0a9484b96d5060cc5ced589656f56c47672
Malena Daniela Farfan Gutierrez <malenafarfan09@gmail.com>
2026-05-08 20:34:29 -0300

    Ejercicio 4 de JS

---
+++ Js/ejercicio4.js
@@ -0,0 +1,17 @@
+/**
+ * TRABAJO PRÁCTICO N°2 - EJERCICIO 4
+ * Consigna: Calcular área y perímetro de un rectángulo (unidad: cm).
+ */
+
+
+const base = 10; 
+const altura = 5;
+
+
+const area = base * altura;
+const perimetro = 2 * (base + altura);
+
+
+console.log(`Datos del rectángulo: Base = ${base}cm, Altura = ${altura}cm`);
+console.log(`El área del rectángulo es: ${area} cm²`);
+console.log(`El perímetro del rectángulo es: ${perimetro} cm`);
