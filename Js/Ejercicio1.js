1e7404d666b5eceecf774e7e02020d984f50d490
Malena Daniela Farfan Gutierrez <malenafarfan09@gmail.com>
2026-05-08 20:09:17 -0300

    Ejercicio 1 de JS funcionando correctamente

---
+++ Js/Ejercicio1.js
@@ -0,0 +1,8 @@
+/**
+ * TRABAJO PRÁCTICO N°2 - EJERCICIO 1
+ * Consigna: Mostrar integrantes usando const o let.
+ */
+
+const integrantes = "Cardenes Matias Nicolas, Farfan Gutierrez Malena Daniela, Figueroa Mariel Ayelen, Piccoletti Facundo Daniel y Rios Carlos Dana Eugenia";
+
+console.log(`Somos ${integrantes} y estamos aprendiendo javascript.`);;
